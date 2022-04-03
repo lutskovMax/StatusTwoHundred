c959f30b855afe1b6a4fc8c89f228f53193e2519
unknown <maksym.lutskov>
2022-04-04 00:51:37 +0300

    first

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__body">
+        <div class="header__logo">
+          <img src="~@/assets/image/logo.png" alt="" />
+        </div>
+        <div class="header__menu">
+          <ul>
+            <nuxt-link to="/login">Питання</nuxt-link>
+            <li>Позначки</li>
+            <li>Учасники</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {
+    localePathAbout() {
+      return this.localePath('login')
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <Header />
+    <h1>Nice</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+
+  mounted() {
+    this.$axios
+      .get('https://webforum.azurewebsites.net/api/Values')
+      .then((r) => {
+        console.log(r.data)
+      })
+  },
+}
+</script>
+<style lang="scss" scoped>
+h1 {
+  color: aqua;
+}
+</style>
